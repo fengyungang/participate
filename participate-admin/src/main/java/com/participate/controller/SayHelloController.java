dc93337f927653c1f816c5116a817b1aacd49afa
fengyungang <fengyungang123@163.com>
2018-11-07 15:31:10 +0800

    修改pom.xml指定mian方代码

--- participate-admin/src/main/java/com/participate/controller/SayHelloController.java
+++ participate-admin/src/main/java/com/participate/controller/SayHelloController.java
@@ -1,6 +1,5 @@
 package com.participate.controller;
 
-import com.participate.SayHello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SayHelloController {
     @RequestMapping("/sayHello")
     public String sayHello(){
-       String aa=SayHello.fyg();
-       return aa;
+
+       return null;
    }
 }
